00b7ceb3ac19b71f333bbecb1de0681052fbc8b8
kateHwang <ghkd6385@gmail.com>
2022-01-16 00:12:34 +0900

    study-JS20220115

--- first-Js/main.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-//상식적인 모습으로 자바스크립트를 유연하게 이용할 수 있도록
-console.log('hello-world');
---
+++ first-Js/variable.js
@@ -0,0 +1,99 @@
+'use strict';
+//상식적인 모습으로 자바스크립트를 유연하게 이용할 수 있도록
+
+
+//2. Variable(변수-변경될 수 있는 값)
+//let (added ES6)-Mutable
+//Block Scope
+let globalName = 'global name'; //global scope
+{
+let name = 'kate';
+console.log(name);
+
+name = 'hellokate';
+console.log(name);
+console.log(globalName);//글로벌변수
+}
+console.log(name);//block Scope
+console.log(globalName);//글로벌변수
+
+//var-nope
+//var hoisting(어디에 선언했나에 상관없이 항상 제일 위로 선언을 
+//끌어올려주는 것을 말한다.)
+//var는 블록스코프가 없다. 블록스코프를 철저히 무시한다.
+
+//3.Constants (Immutable)
+// 왠만하면 값을 할당한 다음에 다시는 변경되지 않는 데이터 타입을 사용해라
+// - 보안성 (security)
+// - 다양한 Thread의 변경가능성 (thread Safety)
+// - 개발자들의 실수를 방지할 수 있음 (reduce human mistakes)
+const daysInWeek = 7;
+const maxNumber = 5;
+
+//4. Variable Types
+//primitive, single item: number, string, boolean, null, undefined, symbol
+//object, box container
+//function, first-class function
+
+const count = 17;
+const size = 17.1;
+console.log(`value: ${count} , type: ${typeof count}`);
+console.log(`value: ${size} ,  type: ${typeof size}`);
+
+//Number - speicla numeric values
+
+//bigInt
+const bigInt = 123456789012345678901234567890n; //over(-2**53) ~2*53)
+console.log(`value: ${bigInt}, type:${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+//String
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;//일반 스트링 + 변수 = 
+console.log(`value: ${greeting}, type:${typeof greeting}`);
+const helloyap = `hi ${brendan}!`; //template literals(String)
+console.log(`value:${helloyap}, type:${typeof helloyap}`);
+
+//boolean
+//false:0, null, undefined, NaN, ''
+//true: 그 외의 값
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type:${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+
+//null
+let nothing = null; //너는 비어있는 값이라고 지정하는거다.
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+//undefined
+let x; //선언은 되었지만 아무것도 값이 지정되어있지 않다.(정해지지않은경우)
+console.log(`value: ${x}, type: ${typeof x}`);
+
+//symbol
+//map이나 다른자료에서 고유한 식별자가 필요할때, 우선순위를 주고싶을때
+const symbol1 = Symbol('id');//주어진 
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gsymbol1 = Symbol.for('id');
+const gsymbol2 = Symbol.for('id');
+console.log(gsymbol1 === gsymbol2); //true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`)
+
+//object
+const kate = { name:'kate', age: 30 }; //const이기때문에 변환불가
+kate.age = 30;
+console.log(`value: ${kate}, type: ${typeof kate}`);
+
+//5. 동적 타입핑(Dynamic typing)
+let text = 'hello';
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text ='7'+5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text ='8'/'2';
+console.log(`value: ${text}, type: ${typeof text}`);
