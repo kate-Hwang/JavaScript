76080bb578a2267f0472cd6f7cbb147c3b2a8685
kateHwang <ghkd6385@gmail.com>
2022-01-18 14:48:49 +0900

    20220118-commit-function

--- first-Js/function.js
+++ first-Js/function.js
@@ -44,4 +44,114 @@
     showMessage('fuckyou!');
 
 //4. Rest parameters -> 배열형태로 전달된다.
+function printAll(...args){
+    for(let i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
 
+    for(const arg of args){ //for ~ of 문구
+        console.log(arg);
+    }
+
+    //상세한 배열설명참고
+    args.forEach((arg) => console.log(arg));
+
+}
+printAll('dream','coding','kate');
+
+//5.Local Scope(지역변수)
+//밖에서는 안이 보이지 않고 안에서는 밖을 볼 수 있다.
+
+    let globalMessage = 'global'; //global 변수
+
+    function printMessage() {
+        let message ='yapping';
+        console.log(message); //local 변수
+        console.log(globalMessage);
+
+            function printAnother(){
+                console.log(message);
+                let childMessage ='im baby_yapp!';
+            }
+            console.log(message);
+    }
+
+    printMessage();
+
+//6. Return a value
+
+    function sum(a,b) {
+        return a + b;
+    }
+    const result = sum(1,2); //값은 3
+    console.log(`sum: ${sum(1,2)}`);
+
+//7. Early return, early exit
+//실무에서 안좋은 로직방향
+    function upgradeUser(user){
+        if(user.point > 10){
+            //long upgrade logic...
+        }
+    }
+//실무에서 좋은 로직방향
+//빨리 리턴을 하고 실행하도록 하는것이 좋다.
+    function upgrade(user){
+        if(user.point > 10){
+            return;
+        }
+        //long upgrade logic.....
+    }
+
+//8.function Expression
+//함수는 변수에 할당이 되고 함수의 파라미터에 변환이 된다.
+//function Expression은 호이스팅(hoisting)이 된다.
+//함수가 선언되기 이전에 호출가능하다.
+    const print = function(){ // 익명함수, 이름을 선언할 수 있음
+        console.log('print');
+    };
+    print();
+    const printAgain = print;
+    printAgain();
+    const sumAgain = sum;
+    console.log(sumAgain(1,3));
+
+//9.Callback 함수
+//
+
+function randomQuiz(answer, printYes, printNo){
+    //if - else구문
+    if(answer ==='love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+const printYes = function (){ //익명함수
+    console.log('yes!');
+};
+
+//지정함수를 쓰는 이유는 디버깅을 할때 함수이름이 나오게 하기위해서, 함수안에서
+//또 다른 스스로의 함수를 호출할때 - 잘 하지말것(필요할때만 할것.)
+const printNo = function print(){ //지정함수(named function)
+    console.log('Nope!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you',printYes, printNo);
+
+//Arrow Function (익명함수로 항상 씀)
+    const simplePrint = function(){
+        console.log('simplePrint');
+    };
+
+    //간결하게 쓸수있고 깔끔하게 표현가능하다.
+    const simplePrint = () => console.log('simplePrint'); //arrow function
+    const add = (a,b) => a + b;
+    const simplePrint = (a,b) => {
+        //do something more
+        return a * b;
+    };
+
+//IIFE : 함수를 바로 불러와서 실행 할 수 있는 방법
+    (function hello(){
+        console.log('IIFE');
+    })();
